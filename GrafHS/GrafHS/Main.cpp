ce8f4d7946180bd5ab92208f77151f4ca783af27
Matias <matias@karplus.com.ar>
2020-05-04 19:12:01 -0300

    Testing object creation

--- GrafHS/GrafHS/Main.cpp
+++ GrafHS/GrafHS/Main.cpp
@@ -2,11 +2,7 @@
 #include <iostream>
 #include <GL/glut.h>
 #include "HighScore.h"
-
-void draw() {
-	glFlush();
-
-}
+using namespace HighScoreListLT;
 
 void drawText(const char *text, int length, int x, int y) {
 	glMatrixMode(GL_PROJECTION);
@@ -31,8 +27,39 @@ void drawText(const char *text, int length, int x, int y) {
 	glMatrixMode(GL_MODELVIEW);
 
 }
+void draw() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	gluLookAt(0,0,-10,0,0,3,0,1,0);
+
+	glColor3f(1,0,0);
+	glBegin(GL_LINES);
+	glVertex3f(0,0,0);
+	glVertex3f(1, 0, 0);
+	glEnd();
+
+	std::string text;
+	text = "Hello world";
+	drawText(text.data(), text.size(), 5, 570);
+
+
+	/*for (int i = 0; i < 5; i++){
+		drawText(HSlist->GetNodeScore(i).name.c_str(),200, 1,1);
+	}*/
+
+	glutSwapBuffers();
+}
+
 
 int main(int argc, char * args[]) {
+
+	HighScore* HSlist = new HighScore(false, 5);
+	/*HSlist->InsertScore(200, "Matias");
+	HSlist->InsertScore(170, "Theo");
+	HSlist->InsertScore(210, "Sofia");
+	HSlist->InsertScore(125, "Ivan");
+	HSlist->InsertScore(305, "Tyler");*/
+	
+
 
 	glutInit(&argc, args);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
@@ -42,7 +69,7 @@ int main(int argc, char * args[]) {
 	glutDisplayFunc(draw);
 
 	glutMainLoop();
-
+	delete HSlist;
 
 	return 0;
 }
